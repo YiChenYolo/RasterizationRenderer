db174c80ba4c16addbc784027fadfe8628060a97
YiChen <17764004602@163.com>
2025-02-05 14:52:26 +0800

    did a optimization for function draw_line

--- main.cpp
+++ main.cpp
@@ -5,36 +5,8 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
-void draw_line(int x1, int y1, int x2, int y2, TGAImage& image,const TGAColor &color) {
-	bool steep = false;
-	if ((x2 - x1) < std::abs(y2 - y1)) {
-		steep = true;
-		std::swap(x1, y1); std::swap(x2, y2);
-	}
-	if (x1 > x2) {
-		std::swap(x1, x2);
-		std::swap(y1, y2);
-	}
 
-	float k = std::abs((float)(y2 - y1) / (x2 - x1));
-	float err = -0.5;
-	int y = y1, x = x1;
-	int dy = y2 > y1 ? 1 : -1;
-	while (true) {
-		if (x == x2) break;
-		if (steep) image.set(y, x, color);
-		else image.set(x, y, color);
-		x++;
-		err += k;
-		//std::cout << err << std::endl;
-		if (err >= 0) {
-			err--;
-			y += dy;
-		}
-	}
-}
-
-void draw_line1(int x1, int y1, int x2, int y2, TGAImage& image, const TGAColor& color) {
+void draw_line(int x1, int y1, int x2, int y2, TGAImage& image, const TGAColor& color) {
 	bool steep = false;
 	//make sure always draws a line with slope lower than 1
 	if ((x2 - x1) < std::abs(y2 - y1)) {
@@ -50,19 +22,34 @@ void draw_line1(int x1, int y1, int x2, int y2, TGAImage& image, const TGAColor&
 	int dx = x2 - x1;
 	int dy = std::abs(y2 - y1);
 	int derr = dy << 1;
-	int err = - dx;
+	int err = -dx;
 	int y = y1, x = x1;
 	int dir = y2 > y1 ? 1 : -1;
-	while (true) {
-		if (x == x2) break;
-		if (steep) image.set(y, x, color);
-		else image.set(x, y, color);
-		x++;
-		err += derr;
-		//std::cout << err << std::endl;
-		if (err >= 0) {
-			err = err - (dx<<1);
-			y += dir;
+
+	if (steep){
+		while (true) {
+			if (x == x2) break;
+			image.set(y, x, color);
+			x++;
+			err += derr;
+			//std::cout << err << std::endl;
+			if (err >= 0) {
+				err = err - (dx << 1);
+				y += dir;
+			}
+		}
+	}
+	else {
+		while (true) {
+			if (x == x2) break;
+			image.set(x, y, color);
+			x++;
+			err += derr;
+			//std::cout << err << std::endl;
+			if (err >= 0) {
+				err = err - (dx << 1);
+				y += dir;
+			}
 		}
 	}
 }
@@ -84,8 +71,8 @@ void effciency_test(TGAImage &image) {
 
 int main(int argc, char** argv) {
 	TGAImage image(100, 100, TGAImage::RGB);
-	draw_line1(77, 81, 74, 98, image, white);
-	//effciency_test(image);
+	draw_line(77, 81, 74, 98, image, white);
+	effciency_test(image);
 	image.flip_vertically();
 	image.write_tga_file("output.tga");
 	return 0;
